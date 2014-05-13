5188cfeefc7513356c1800243e73e95ddcbb5e6f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-12 22:30:08 -0500

    Add Config.merge

--- lib/conform_config.ex
+++ lib/conform_config.ex
@@ -1,6 +1,7 @@
 defmodule Conform.Config do
   @moduledoc """
-  This module is responsible for reading and writing *.config files
+  This module is responsible for reading and writing,
+  and manipulating *.config files
   """
 
   @doc """
@@ -17,6 +18,18 @@ defmodule Conform.Config do
   @spec write(binary, term) :: :ok | {:error, term}
   def write(path, config) do
     '#{path}' |> :file.write_file(:io_lib.fwrite('~p.\n', [config]))
+  end
+
+  @doc """
+  Merge two configs together to produce a new unified config.
+  The second argument represents the config with the highest precedence
+  in the case of conflicts.
+  """
+  @spec merge(Keyword.t, Keyword.t) :: Keyword.t
+  def merge(config1, config2) do
+    Keyword.merge(config1, config2, fn _, app1, app2 ->
+      Keyword.merge(app1, app2)
+    end)
   end
 
 end
