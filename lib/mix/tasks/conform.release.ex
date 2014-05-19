86d2d1f3c1f173333191d44da2c07a5691e971f3
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-19 14:36:58 -0500

    Add internal release task

---
+++ lib/mix/tasks/conform.release.ex
@@ -0,0 +1,23 @@
+defmodule Mix.Tasks.Conform.Release do
+  @moduledoc """
+  This is an internal mix task, meant to be consumed via exrm. Do not use.
+  """
+  @shortdoc false
+
+  use Mix.Task
+
+  def run(_) do
+    conform_path = Path.join([__DIR__, "..", "..", ".."]) |> Path.expand
+    escript_path = Path.join(conform_path, "conform")
+    current_path = File.cwd!
+    # Switch to conform directory
+    File.cd! conform_path
+    # Run escript task
+    result = case Mix.Shell.cmd("mix escriptize", fn _ -> nil end) do
+      0 -> escript_path
+      _ -> {:error, "Failed to generate escript."}
+    end
+    File.cd! current_path
+    result
+  end
+end
