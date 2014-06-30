b5547885689d7928c445d9429c8b11b5586678a4
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-06-29 20:11:50 -0500

    Update to elixir v0.14.2

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Conform.Mixfile do
 
   def project do
     [app: :conform,
-     version: "0.7.3",
-     elixir: "~> 0.14.2-dev",
+     version: "0.7.4",
+     elixir: "~> 0.14.2",
      escript: [main_module: Conform],
      description: description,
      package: package,
