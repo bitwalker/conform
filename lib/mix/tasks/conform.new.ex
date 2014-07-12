30dc5d8961ebcf3ae2a8434cbdf5202349e5e910
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-07-11 22:18:40 -0500

    Fix issue with schema generation ignoring environment. Closes #4

--- lib/mix/tasks/conform.new.ex
+++ lib/mix/tasks/conform.new.ex
@@ -15,48 +15,24 @@ defmodule Mix.Tasks.Conform.New do
   def run(_args) do
     if Mix.Project.umbrella? do
       config = [build_path: Mix.Project.build_path]
-      # Collect a list of absolute paths to all config files for the umbrella
-      app_configs =
-        Mix.Dep.Umbrella.loaded
-        |> Enum.flat_map(fn %Mix.Dep{app: app, opts: opts} ->
-            Mix.Project.in_project(app, opts[:path], config, fn _ -> Mix.Project.config_files end)
-            |> Enum.reject(&filter_config/1)
-            |> Enum.map(fn c -> {app, Path.join(opts[:path], c) |> Path.expand} end)
-           end)
       # Execute task for each project in the umbrella
       for %Mix.Dep{app: app, opts: opts} <- Mix.Dep.Umbrella.loaded do
-        Mix.Project.in_project(app, opts[:path], config, fn _ -> do_run(app_configs) end)
+        Mix.Project.in_project(app, opts[:path], config, fn _ ->
+          do_run(app, Path.expand("config/config.exs"))
+        end)
       end
     else
       app = Mix.Project.config |> Keyword.get(:app)
-      Mix.Project.config_files
-      |> Enum.reject(&filter_config/1)
-      |> Enum.map(fn c -> {app, c |> Path.expand} end)
-      |> do_run
+      do_run(app, Path.expand("config/config.exs"))
     end
   end
 
-  defp do_run(app_configs) do
-    app         = Mix.Project.config |> Keyword.get(:app)
+  defp do_run(app, config_path) do
     output_path = Path.join([File.cwd!, "config", "#{app}.schema.exs"])
-
-    # Load the configuration for this app, and any apps which this app depends on
-    deps = [{app, []} | (Mix.Project.config |> Keyword.get(:deps))]
-    config = app_configs |> Enum.reduce([], fn {application, config_path}, result ->
-      case deps |> Keyword.get(application) do
-        nil ->
-          result
-        _ ->
-          # Read in the config at `config_path`
-          loaded = Mix.Config.read!(config_path)
-          # Merge the loaded config over the current config stored in `result`
-          Mix.Config.merge(result, loaded)
-      end
-    end)
-
+    # Load the configuration for this app
+    config = Mix.Config.read!(config_path)
     # Convert configuration to schema format
     schema = Conform.Schema.from_config(config)
-
     # Output configuration to `output_path`
     output_path |> File.write!(schema |> stringify)
     info "The generated schema for your project has been placed in config/#{app}.schema.exs"
@@ -79,10 +55,6 @@ defmodule Mix.Tasks.Conform.New do
         |> String.replace("translations: []]", " translations: []\n]")
       Regex.replace(~r/\s+(\".*\"\: \[)/, contents, "\n    \\1")
     end
-  end
-
-  defp filter_config(config) do
-    config |> String.ends_with?([".conf", ".schema.exs", "mix.exs", ".lock"])
   end
 
 end
