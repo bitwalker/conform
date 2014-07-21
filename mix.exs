3c9d4668e350ff73a3b444409e241243f30239ce
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-07-20 23:44:15 -0500

    Version 0.8.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Conform.Mixfile do
 
   def project do
     [app: :conform,
-     version: "0.8.5",
+     version: "0.8.6",
      elixir: "~> 0.14.3",
      escript: [main_module: Conform],
      description: description,
