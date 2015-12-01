02f2440f49c439b77918bbd275b05d3f2ba4119f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-12-01 16:00:25 -0600

    Do not include ex_doc or earmark in package deps

--- mix.exs
+++ mix.exs
@@ -25,13 +25,15 @@ defmodule Conform.Mixfile do
   defp compilers(true), do: [:peg, :erlang, :elixir, :app]
   defp compilers(_),    do: nil
 
-  defp deps(true), do: [{:neotoma, "~> 1.7.3"}] ++ core_deps()
-  defp deps(_),    do: core_deps()
+  defp deps(true) do
+    [{:neotoma, "~> 1.7.3"},
+     {:ex_doc, "~> 0.10", only: [:docs, :dev]},
+     {:earmark, "> 0.0.0", only: [:docs, :dev]}] ++ core_deps()
+  end
+  defp deps(_), do: core_deps()
 
   defp core_deps(), do: [
     {:exrm, "~> 1.0.0-rc5", optional: true},
-    {:ex_doc, "~> 0.7", only: [:docs, :dev]},
-    {:earmark, "~> 0.1", only: [:docs, :dev]}
   ]
 
   defp description, do: "Easy release configuration for Elixir apps."
