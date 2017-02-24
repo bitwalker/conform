c39ce226e06779dbe97b21a36ddd80df2a8c8f5b
Paul Schoenfelder <paulschoenfelder@gmail.com>
2017-02-24 11:40:15 -0600

    Version 2.2.0

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Conform.Mixfile do
 
   def project do
     [app: :conform,
-     version: "2.1.2",
+     version: "2.2.0",
      elixir: "~> 1.0",
      escript: [main_module: Conform],
      compilers: compilers(@do_peg_compile?),
