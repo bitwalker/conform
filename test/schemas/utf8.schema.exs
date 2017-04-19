510042d8b5c8458501839b9591fb3fd4a12d1bdd
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2017-04-19 20:45:44 +0200

    Add a configuration with an UTF-8 character

---
+++ test/schemas/utf8.schema.exs
@@ -0,0 +1,16 @@
+[
+  extends: [],
+  import: [],
+  mappings: [
+    "utf8": [
+      commented: false,
+      datatype: :binary,
+      default: "Fixé",
+      doc: "A string with an é.",
+      hidden: false,
+      to: "my_app.uft8"
+    ],
+  ],
+  transforms: [],
+  validators: []
+]
